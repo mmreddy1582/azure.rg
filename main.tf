9d5b4b15b77141af1658e701af8ccd4a89e83b87
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2020-04-10 19:46:55 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
     version = "=2.5.0"
     features {}
     }
-resource "azurerm_resource_group" "web-rg5" {
-    name = "web-rg5"
+resource "azurerm_resource_group" "web-rg6" {
+    name = "web-rg6"
     location = "eastus"
 }
